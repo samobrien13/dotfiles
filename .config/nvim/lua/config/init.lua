ee567837308387ba7ba44e6d1424540d4eeaf99e
Sam O'Brien <sam@getmosh.com>
2025-05-10 20:15:59 +1000

    Upgrade to neovim 11

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -4,13 +4,19 @@ require("config.lazy")
 
 local augroup = vim.api.nvim_create_augroup
 local ThePrimeagenGroup = augroup('ThePrimeagen', {})
-
+local getClient = vim.lsp.get_client_by_id
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
 function R(name)
     require("plenary.reload").reload_module(name)
 end
+
+vim.filetype.add({
+    extension = {
+        templ = 'templ',
+    }
+})
 
 autocmd('TextYankPost', {
     group = yank_group,
@@ -23,17 +29,44 @@ autocmd('TextYankPost', {
     end,
 })
 
+
 autocmd({ "BufWritePre" }, {
     group = ThePrimeagenGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
 
-autocmd({ "BufWritePre" }, {
-    pattern = "*",
-    callback = function(args) vim.lsp.buf.format() end,
+autocmd('LspAttach', {
+    group = ThePrimeagenGroup,
+    callback = function(args)
+        local client = assert(getClient(args.data.client_id))
+        local opts = { buffer = args.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
 })
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+vim.lsp.config('luals', {
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    root_markers = { '.luarc.json', '.luarc.jsonc' },
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim', 'ColorMyPencils' }
+            }
+        }
+    }
+})

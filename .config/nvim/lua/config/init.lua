6e74cc65034896692ecf855cab220016274d92ed
Sam O'Brien <sam@getmosh.com>
2025-05-11 14:48:26 +1000

    Switch to conform

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -47,14 +47,14 @@ autocmd('LspAttach', {
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     end
 })
 
 autocmd({ "BufWritePre" }, {
     pattern = "*",
-    callback = function() vim.lsp.buf.format() end,
+    callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+    end,
 })
 
 vim.g.netrw_browse_split = 0
---
+++ .config/nvim/lua/plugins/format.lua
@@ -0,0 +1,34 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            "<leader>f",
+            function()
+                require("conform").format({ async = true })
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
+    },
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+            ruby = { "rubocop" },
+        },
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+        format_on_save = { timeout_ms = 500 },
+        formatters = {
+            shfmt = {
+                prepend_args = { "-i", "2" },
+            },
+        },
+    },
+    init = function()
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+}

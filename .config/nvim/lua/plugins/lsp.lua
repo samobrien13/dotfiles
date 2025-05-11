b7e77959ff74173a75dd912c1ea60d30230c9e41
Sam O'Brien <sam@getmosh.com>
2025-05-11 14:32:05 +1000

    Update to blink completions

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,37 +3,48 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
+        {
+            'saghen/blink.cmp',
+            dependencies = { 'rafamadriz/friendly-snippets' },
+            version = '1.*',
+            opts = {
+                keymap = { preset = 'default' },
+                appearance = {
+                    nerd_font_variant = 'mono'
+                },
+                fuzzy = { implementation = "prefer_rust_with_warning" }
+            },
+        },
+        {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
     },
 
     config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
             "force",
             {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+            vim.lsp.protocol.make_client_capabilities()
+        )
 
-        require("fidget").setup({})
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
-                    'cssmodules_ls',
-                    'eslint',
-                    'lua_ls',
-                    'rust_analyzer',
-                    'ts_ls',
-                    'yamlls',
-                    'ruby_lsp',
-                    'rubocop',
+                'cssmodules_ls',
+                'tailwindcss',
+                'eslint',
+                'lua_ls',
+                'rust_analyzer',
+                'ts_ls',
+                'yamlls',
+                'ruby_lsp',
+                'rubocop',
             },
             handlers = {
                 function(server_name) -- default handler (optional)
@@ -42,18 +53,8 @@ return {
                     }
                 end,
 
-                ["lua_ls"] = function()
+                ['yamlls'] = function()
                     local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
                     lspconfig.yamlls.setup {
                         settings = {
                             yaml = {
@@ -63,41 +64,6 @@ return {
                     }
                 end,
             }
-        })
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
-            })
-        })
-
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            virtual_text = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
         })
     end
 }

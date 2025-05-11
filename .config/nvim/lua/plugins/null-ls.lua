0d146053b97d843c4c79e279caa6febef5d7e780
Sam O'Brien <sam@getmosh.com>
2025-05-11 20:05:19 +1000

    Remove plugin afters

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,3 +1,14 @@
 return {
-    "nvimtools/none-ls.nvim"
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        local b = null_ls.builtins
+
+        null_ls.setup({
+            sources = {
+                b.formatting.prettier,
+            },
+        })
+    end
+
 }

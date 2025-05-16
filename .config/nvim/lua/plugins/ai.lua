59f077a3f31ef30c68843adcc4ebfe4d114f6f66
Sam O'Brien <sam@getmosh.com>
2025-05-16 13:50:40 +1000

    Add copilot to blink

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -21,14 +21,7 @@ return {
 			config = function()
 				require("copilot").setup({
 					suggestion = {
-						enabled = true,
-						auto_trigger = true,
-						hide_during_completion = false,
-						debounce = 75,
-						trigger_on_accept = true,
-						keymap = {
-							accept = "<Tab>",
-						},
+						enabled = false,
 					},
 				})
 			end,
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -7,26 +7,58 @@ return {
 		{
 			"saghen/blink.cmp",
 			dependencies = {
+				{ "L3MON4D3/LuaSnip", version = "v2.*" },
 				"Kaiser-Yang/blink-cmp-avante",
+				"giuxtaposition/blink-cmp-copilot",
 				"rafamadriz/friendly-snippets",
+				"solidjs-community/solid-snippets",
 			},
 			version = "1.*",
 			opts = {
-				default = { "avante", "lsp", "path", "luasnip", "buffer" },
-				providers = {
-					avante = {
-						module = "blink-cmp-avante",
-						name = "Avante",
-						opts = {
-							-- options for blink-cmp-avante
+				snippets = {
+					expand = function(snippet)
+						require("luasnip").lsp_expand(snippet)
+					end,
+					active = function(filter)
+						if filter and filter.direction then
+							return require("luasnip").jumpable(filter.direction)
+						end
+						return require("luasnip").in_snippet()
+					end,
+					jump = function(direction)
+						require("luasnip").jump(direction)
+					end,
+				},
+				sources = {
+					default = { "copilot", "avante", "lsp", "path", "buffer" },
+					providers = {
+						avante = {
+							module = "blink-cmp-avante",
+							name = "Avante",
+						},
+						copilot = {
+							name = "copilot",
+							module = "blink-cmp-copilot",
+							enabled = true,
+							score_offset = 100,
+							async = true,
+							transform_items = function(_, items)
+								local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+								local kind_idx = #CompletionItemKind + 1
+								CompletionItemKind[kind_idx] = "Copilot"
+								for _, item in ipairs(items) do
+									item.kind = kind_idx
+								end
+								return items
+							end,
 						},
 					},
+					keymap = { preset = "default" },
+					appearance = {
+						nerd_font_variant = "mono",
+					},
+					fuzzy = { implementation = "prefer_rust_with_warning" },
 				},
-				keymap = { preset = "default" },
-				appearance = {
-					nerd_font_variant = "mono",
-				},
-				fuzzy = { implementation = "prefer_rust_with_warning" },
 			},
 		},
 		{

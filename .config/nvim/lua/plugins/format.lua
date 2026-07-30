5065712f74ee461fbf33013aaf7438cf9533dffa
Sam O'Brien <sam@getmosh.com>
2026-07-30 16:21:53 +1000

    Increase timeout

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -32,7 +32,7 @@ return {
 		default_format_opts = {
 			lsp_format = "fallback",
 		},
-		format_on_save = { timeout_ms = 500 },
+		format_on_save = { timeout_ms = 2000 },
 		formatters = {
 			shfmt = {
 				append_args = { "-i", "2" },
